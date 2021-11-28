3e74fd7e19bf1a5e3a62885837692bbda283abd3
herbie-dev <dev.herbiejohn@gmail.com>
2021-11-28 09:23:46 +0000

    Update : Add Google Maps Api

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,70 @@
 @extends('layout.app')
 @section('content')
 <div class="container-fluid">
+  <br>
+  <center><h3>Click location to check weather</h3></center>
   <div class="fade-in">
-    
+    <div id="map" class="col-12" style="width:100%;height:400px;"></div>
   </div>
-  <h3>Click location to check weather</h3>
 </div>
 
 @endsection
 @push('scripts')
+  <script>
+
+
+  var marker;
+  var infowindow;
+  var geocoder;
+  var mapCanvas = document.getElementById("map");
+  var myCenter=new google.maps.LatLng(11.5776725,113.5671426);
+  var mapOptions = {
+        center: myCenter, zoom: 5,
+        mapTypeId:google.maps.MapTypeId.ROADMAP
+      };
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+          google.maps.event.addListener(map, 'click', function(event) {
+          placeMarker(map, event.latLng);
+        });
+
+        $.ajaxSetup({
+          headers: {
+            'X-Requested-With': 'XMLHttpRequest',
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+          }
+        });
+
+
+
+        function placeMarker(map, location) {
+          $('#btnweather').removeAttr("disabled")
+          var lat = location.lat();
+          var longit = location.lng();
+          var apiKey = "73d912b5e150eba0a6334a3ebaeed7ce";
+          var activeInfoWindow;
+
+          if (!marker || !marker.setPosition) {
+              marker = new google.maps.Marker({
+                  position: location,
+                  map: map,
+              });
+
+          } else {
+              marker.setPosition(location);
+          }
+
+          if (!!infowindow && !!infowindow.close) {
+              infowindow.close();
+          }
+
+                infowindow = new google.maps.InfoWindow();
+                infowindow.setContent('Latitude: ' + location.lat() + '<br>Longitude: ' + location.lng());
+                infowindow.open(map, marker);
+
+      }
+
+
+
+  </script>
 
 @endpush
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -25,6 +25,7 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBy5reV5QpkPAx7oUZYZDWXNzOj12ybWVE&libraries=&v=weekly"></script>
 
 @stack('scripts')
 
